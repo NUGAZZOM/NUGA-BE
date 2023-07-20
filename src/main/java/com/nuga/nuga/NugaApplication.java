0ee4f3dd79bd1d79e043de86b93a8cebc1fe4ec5
onlee1425 <enflenffl@gmail.com>
2023-07-20 20:26:28 +0900

    init

---
+++ src/main/java/com/nuga/nuga/NugaApplication.java
@@ -0,0 +1,13 @@
+package com.nuga.nuga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NugaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuga/nuga/NugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuga.nuga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
